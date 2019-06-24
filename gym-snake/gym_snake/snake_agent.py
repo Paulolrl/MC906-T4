b11d9a248f317163c53a011d7c95d446f4ab0892
Juliana Okubo <40123319+JukiOK@users.noreply.github.com>
2019-06-24 16:32:07 -0300

    Update snake_agent.py

--- gym-snake/gym_snake/snake_agent.py
+++ gym-snake/gym_snake/snake_agent.py
@@ -24,3 +24,16 @@ model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
 print(model.summary())
+
+policy = EpsGreedyQPolicy()
+memory = SequentialMemory(limit=50000, window_length=1)
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
+target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+
+# Training time
+dqn.fit(env, nb_steps=5000, visualize=True, verbose=2)
+
+# Test time
+dqn.test(env, nb_episodes=5, visualize=True)
+
