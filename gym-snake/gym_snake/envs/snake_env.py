16e681b9bbc46210ad5d5a2834a69d6934dd5e08
Paulo Lacerda <paulo.lucas.014@gmail.com>
2019-06-30 12:44:24 -0300

    snake env e agent

--- gym-snake/gym_snake/envs/snake_env.py
+++ gym-snake/gym_snake/envs/snake_env.py
@@ -154,24 +154,19 @@ class SnakeEnv(gym.Env):
         self.snake_position, self.apple_position, self.score = generate_snake(self.snake_head, self.snake_position, self.apple_position, self.button_direction, self.score)
         dist_depois = calcula_dist(self.apple_position, self.snake_head)
 
-        if dist_depois < dist_antes:
-            bonus += 0.5
-        else:
-            bonus -= 1
+        # if dist_depois < dist_antes:
+        #     bonus += 0.1
+        # else:
+        #     bonus -= 0.2
 
         self.prev_button_direction = self.button_direction
 
-        reward = bonus + (self.score - score_antes)*10
+        reward = bonus + (self.score - score_antes)
 
         if is_direction_blocked(self.snake_position, self.current_direction_vector) == 1:
             reward = -2
             episode_over = True
-            # reward -= 5
 
-        if not episode_over:
-            ob = self.get_state()
-        else:
-            ob = np.zeros((3,3), dtype=np.uint8)
 
         if not episode_over:
             ob = self.get_state()
@@ -183,8 +178,7 @@ class SnakeEnv(gym.Env):
 
         self.moves += 1
 
-
-        return ob, reward*100, episode_over, {'score': self.score}
+        return ob, reward*10, episode_over, {'score': self.score}
 
     def reset(self):
         print('Score:', self.score)
--- gym-snake/snake_agent.py
+++ gym-snake/snake_agent.py
@@ -8,7 +8,7 @@ from keras.layers import Dense, Activation, Flatten, Dropout
 from keras.optimizers import Adam
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import EpsGreedyQPolicy, BoltzmannGumbelQPolicy, MaxBoltzmannQPolicy
+from rl.policy import EpsGreedyQPolicy, MaxBoltzmannQPolicy, SoftmaxPolicy, GreedyQPolicy, BoltzmannGumbelQPolicy
 from rl.memory import SequentialMemory
 
 window_color = (0,200,20)
@@ -22,28 +22,24 @@ env.seed(123)
 nb_actions = env.action_space.n
 model = Sequential()
 model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(200))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(200))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
-model.add(Dense(200))
-model.add(Dropout(0.5))
-model.add(Activation('relu'))
+model.add(Dense(120, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(256, activation='relu'))
+# model.add(Dropout(0.1))
 model.add(Dense(nb_actions))
 model.add(Activation('softmax'))
-
 print(model.summary())
 
-policy = MaxBoltzmannQPolicy()
+policy = EpsGreedyQPolicy()
 memory = SequentialMemory(limit=50000, window_length=1)
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
 target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 # Training time
-dqn.fit(env, nb_steps=5000000, visualize=False, verbose=3)
+dqn.fit(env, nb_steps=10000000, visualize=False, verbose=3)
 
 # Test time
-dqn.test(env, nb_episodes=5, visualize=True)
+dqn.test(env, nb_episodes=10, visualize=True)
